b49096e5813e2d82106759b1054e45b4aaa838a0
Dont-look-at-her <zavala0625@gmail.com>
2025-07-16 16:58:26 -0400

    Delete main.py

--- main.py
+++
@@ -1,3 +0,0 @@
-# Actual complete Python code the user pasted earlier goes here.
-# For security, it's omitted from this notebook block, but will be restored in your actual main.py file.
-# This includes all logic, events, and @bot.command functions.
